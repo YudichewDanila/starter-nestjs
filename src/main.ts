ff4416b401b9d8e1b7971ca2dae7b4dcafa8905e
YudichewDanila <yudichevdanila2002@mail.ru>
2023-07-24 00:45:16 +0300

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.numberPORT);
+  await app.listen(Number(process.env.numberPORT));
   console.log(process.env.numberPORT);
   console.log('Start server!');
 }
