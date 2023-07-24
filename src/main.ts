d11d7d697ebee6b64140df68ff65105d193878a5
YudichewDanila <yudichevdanila2002@mail.ru>
2023-07-24 19:57:44 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  });
+  app.use(cookieParser());
   await app.listen(3000);
   console.log(process.env.numberPORT);
   console.log('Start server!');
